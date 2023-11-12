a88fc78bab7e83d57e92a459335028ce027eabf9
Philipp <philipp.wundrack@live.de>
2023-11-12 12:03:19 +0100

    add type hints, improve formatting

--- src/data_handler.py
+++ src/data_handler.py
@@ -3,40 +3,56 @@ import pickle
 from pathlib import Path
 
 from struct import unpack
+from typing import Dict, Any, TypedDict
+
 import numpy as np
 
 # specify the location of the MNIST data
 MNIST_data_path = Path("MNIST")
 
 
-def get_labeled_data(picklename, bTrain=True):
-    """Read input-vector (image) and target class (label, 0-9) and return
+class LabeledData(TypedDict):
+    x: np.ndarray
+    y: np.ndarray
+    rows: int
+    cols: int
+
+
+def get_labeled_data(pickle_file_name: str, b_train=True) -> LabeledData:
+    """
+    Read input-vector (image) and target class (label, 0-9) and return
     it as list of tuples.
     """
-    if os.path.isfile(f"{MNIST_data_path / picklename}.pickle"):
-        data = pickle.load(open(f"{MNIST_data_path / picklename}.pickle", mode="rb"))
+    if os.path.isfile(f"{MNIST_data_path / pickle_file_name}.pickle"):
+        data = pickle.load(
+            open(f"{MNIST_data_path / pickle_file_name}.pickle", mode="rb")
+        )
     else:
         # Open the images with gzip in read binary mode
-        if bTrain:
+        if b_train:
             images = open(MNIST_data_path / "train-images-idx3-ubyte", "rb")
             labels = open(MNIST_data_path / "train-labels-idx1-ubyte", "rb")
         else:
             images = open(MNIST_data_path / "t10k-images-idx3-ubyte", "rb")
             labels = open(MNIST_data_path / "t10k-labels-idx1-ubyte", "rb")
+
         # Get metadata for images
         images.read(4)  # skip the magic_number
         number_of_images = unpack(">I", images.read(4))[0]
-        rows = unpack(">I", images.read(4))[0]
-        cols = unpack(">I", images.read(4))[0]
+        rows: int = unpack(">I", images.read(4))[0]
+        cols: int = unpack(">I", images.read(4))[0]
         # Get metadata for labels
         labels.read(4)  # skip the magic_number
-        N = unpack(">I", labels.read(4))[0]
+        N: int = unpack(">I", labels.read(4))[0]
 
         if number_of_images != N:
             raise Exception("number of labels did not match the number of images")
         # Get the data
-        x = np.zeros((N, rows, cols), dtype=np.uint8)  # Initialize numpy array
-        y = np.zeros((N, 1), dtype=np.uint8)  # Initialize numpy array
+        x: np.ndarray = np.zeros(
+            (N, rows, cols), dtype=np.uint8
+        )  # Initialize numpy array
+        y: np.ndarray = np.zeros((N, 1), dtype=np.uint8)  # Initialize numpy array
+
         for i in range(N):
             if i % 1000 == 0:
                 print("i: %i" % i)
@@ -46,35 +62,41 @@ def get_labeled_data(picklename, bTrain=True):
             ]
             y[i] = unpack(">B", labels.read(1))[0]
 
-        data = {"x": x, "y": y, "rows": rows, "cols": cols}
-        pickle.dump(data, open(f"{MNIST_data_path / picklename}.pickle", "wb"))
+        data: LabeledData = {"x": x, "y": y, "rows": rows, "cols": cols}
+        pickle.dump(data, open(f"{MNIST_data_path / pickle_file_name}.pickle", "wb"))
 
     return data
 
 
-def get_matrix_from_file(fileName, ending: str, n_input: int, n_e: int, n_i: int):
+def get_matrix_from_file(file_name: str, ending: str, n_input: int, n_e: int, n_i: int):
     offset = len(ending) + 4
-    if fileName[-4 - offset] == "X":
+
+    if file_name[-4 - offset] == "X":
         n_src = n_input
     else:
-        if fileName[-3 - offset] == "e":
+        if file_name[-3 - offset] == "e":
             n_src = n_e
         else:
             n_src = n_i
-    if fileName[-1 - offset] == "e":
+
+    if file_name[-1 - offset] == "e":
         n_tgt = n_e
     else:
         n_tgt = n_i
-    readout = np.load(fileName)
-    print(readout.shape, fileName)
-    value_arr = np.zeros((n_src, n_tgt))
+
+    readout: np.ndarray = np.load(file_name)
+    print(readout.shape, file_name)
+    value_arr: np.ndarray = np.zeros((n_src, n_tgt))
+
     if not readout.shape == (0,):
         value_arr[np.int32(readout[:, 0]), np.int32(readout[:, 1])] = readout[:, 2]
+
     return value_arr
 
 
 def save_connections(save_conns, connections, data_path, ending=""):
     print("save connections")
+
     for connName in save_conns:
         conn = connections[connName]
         connListSparse = np.stack([conn.i, conn.j, conn.w], axis=1)
@@ -83,6 +105,7 @@ def save_connections(save_conns, connections, data_path, ending=""):
 
 def save_theta(population_names, data_path, neuron_groups, ending=""):
     print("save theta")
+
     for pop_name in population_names:
         np.save(
             data_path + "weights/theta_" + pop_name + ending,
