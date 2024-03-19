fb267d2f42089c885766301e355f5543eaf9d0ee
Philipp <philipp.wundrack@live.de>
2024-03-19 09:44:27 +0100

    add synapse state logging to example

--- src/examples/neuron_state_plot.py
+++ src/examples/neuron_state_plot.py
@@ -11,12 +11,15 @@ from src.hyperparameters import (
 
 
 def main():
+    test_mode = False
     neuron_hyper = NeuronModelHyperparameters.get_default()
-    model_equations = ModelEquations.get_default(test_mode=False)
+    model_equations = ModelEquations.get_default(test_mode=test_mode)
     syn_hyper = SynapseModelHyperparameters.get_default()
 
     input_generator = SpikeGeneratorGroup(
-        2, b2.array([0, 1]), b2.array([5, 10]) * b2.ms
+        2,
+        b2.array([0, 1] + [0] * 110),
+        b2.array([5, 60] + list(range(70, 70 + 110))) * b2.ms,
     )
     neurons = b2.NeuronGroup(
         1,
@@ -26,13 +29,28 @@ def main():
         reset=model_equations.scr_e,
         method="euler",
     )
-    excitatory_synapses = b2.Synapses(
-        input_generator,
-        neurons,
-        model=model_equations.syn_eqs,
-        on_pre=model_equations.syn_eqs_pre_e,
-        on_post=model_equations.syn_eqs_post,
-    )
+
+    if test_mode:
+        excitatory_synapses = b2.Synapses(
+            input_generator,
+            neurons,
+            model=model_equations.syn_eqs,
+            on_pre=model_equations.syn_eqs_pre_e,
+            on_post=model_equations.syn_eqs_post,
+            method="euler",
+        )
+    else:
+        excitatory_synapses = b2.Synapses(
+            input_generator,
+            neurons,
+            model=model_equations.syn_eqs + model_equations.eqs_stdp_ee,
+            on_pre=model_equations.syn_eqs_pre_e
+            + "; "
+            + model_equations.eqs_stdp_pre_ee,
+            on_post=model_equations.syn_eqs_post + model_equations.eqs_stdp_post_ee,
+            method="euler",
+        )
+
     inhibitory_synapses = b2.Synapses(
         input_generator,
         neurons,
@@ -44,13 +62,16 @@ def main():
     excitatory_synapses.connect(condition=True)
     inhibitory_synapses.connect(condition=True)
 
-    weights_e = np.array([[40.0], [0.0]])
-    weights_i = np.array([[0.0], [30.0]])
+    weights_e = np.array([[0.8], [0.0]])
+    weights_i = np.array([[0.0], [10.0]])
 
     excitatory_synapses.w = weights_e[excitatory_synapses.i, excitatory_synapses.j]
     inhibitory_synapses.w = weights_i[inhibitory_synapses.i, inhibitory_synapses.j]
 
-    monitor = b2.StateMonitor(neurons, ["v", "theta", "ge", "gi"], record=True)
+    neuron_monitor = b2.StateMonitor(neurons, ["v", "theta", "ge", "gi"], record=True)
+    synapse_monitor = b2.StateMonitor(
+        excitatory_synapses, ["w", "post2before", "pre", "post1", "post2"], record=True
+    )
     variable_namespace = ModelEquations.create_variable_namespace(
         neuron_hyper, syn_hyper
     )
@@ -58,42 +79,57 @@ def main():
     neurons.v = neuron_hyper.v_rest_e + neuron_hyper.v_start_offset
     neurons.theta = neuron_hyper.theta_start
 
-    b2.run(100 * b2.ms, namespace=variable_namespace)
+    b2.run(200 * b2.ms, namespace=variable_namespace)
 
-    # b2.plot(monitor.t / b2.ms, monitor.v[0] / b2.mV)
+    # b2.plot(neuron_monitor.t / b2.ms, neuron_monitor.v[0] / b2.mV)
     # b2.xlabel("Time (ms)")
     # b2.ylabel("v (mV)")
     # b2.show()
     #
-    # b2.plot(monitor.t / b2.ms, monitor.theta[0] / b2.mV)
+    # b2.plot(neuron_monitor.t / b2.ms, neuron_monitor.theta[0] / b2.mV)
     # b2.xlabel("Time (ms)")
     # b2.ylabel("theta (mV)")
     # b2.show()
     #
-    # b2.plot(monitor.t / b2.ms, monitor.ge[0])
+    # b2.plot(neuron_monitor.t / b2.ms, neuron_monitor.ge[0])
     # b2.xlabel("Time (ms)")
     # b2.ylabel("ge")
     # b2.show()
     #
-    # b2.plot(monitor.t / b2.ms, monitor.gi[0])
+    # b2.plot(neuron_monitor.t / b2.ms, neuron_monitor.gi[0])
     # b2.xlabel("Time (ms)")
     # b2.ylabel("gi")
     # b2.show()
 
     mlflow.set_tracking_uri("http://localhost:5000")
-    mlflow.start_run(experiment_id="8", tags={"implementation": "python"})
+    mlflow.start_run(experiment_id="9", tags={"implementation": "python"})
 
-    for i, v in enumerate(monitor.v[0]):
-        mlflow.log_metric("v", v, step=i)
+    for i, v in enumerate(neuron_monitor.v[0]):
+        mlflow.log_metric("neuron_v", v, step=i)
 
-    for i, theta in enumerate(monitor.theta[0]):
-        mlflow.log_metric("theta", theta, step=i)
+    for i, theta in enumerate(neuron_monitor.theta[0]):
+        mlflow.log_metric("neuron_theta", theta, step=i)
 
-    for i, ge in enumerate(monitor.ge[0]):
-        mlflow.log_metric("ge", ge, step=i)
+    for i, ge in enumerate(neuron_monitor.ge[0]):
+        mlflow.log_metric("neuron_ge", ge, step=i)
 
-    for i, gi in enumerate(monitor.gi[0]):
-        mlflow.log_metric("gi", gi, step=i)
+    for i, gi in enumerate(neuron_monitor.gi[0]):
+        mlflow.log_metric("neuron_gi", gi, step=i)
+
+    for i, w in enumerate(synapse_monitor.w[0]):
+        mlflow.log_metric("exc_syn_w", w, step=i)
+
+    for i, post2before in enumerate(synapse_monitor.post2before[0]):
+        mlflow.log_metric("exc_syn_post2before", post2before, step=i)
+
+    for i, pre in enumerate(synapse_monitor.pre[0]):
+        mlflow.log_metric("exc_syn_pre", pre, step=i)
+
+    for i, post1 in enumerate(synapse_monitor.post1[0]):
+        mlflow.log_metric("exc_syn_post1", post1, step=i)
+
+    for i, post2 in enumerate(synapse_monitor.post2[0]):
+        mlflow.log_metric("exc_syn_post2", post2, step=i)
 
     mlflow.end_run()
 
